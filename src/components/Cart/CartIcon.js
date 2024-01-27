4ee9ff20beea2d9bd78502b9d1ade0037ef7dfd5
Ankit Manjhi <ankitmanjhi286@gmail.com>
2024-01-27 20:37:40 +0530

    Building the header of your restaurant app

---
+++ src/components/Cart/CartIcon.js
@@ -0,0 +1,15 @@
+const cartIcon = () => {
+    return (
+        <svg
+        xmlns='https://ww.w3.org/2000/svg'
+        viewBox='0 0 20 20'
+        fill='currentColor'
+        >
+        
+        <path d='M3 lal l 000 2h1.221.305 1.222a.997.997 0 00.01.042l1.358 5.43-,.893.'></path>
+
+        </svg>
+    )
+}
+
+export default cartIcon;
