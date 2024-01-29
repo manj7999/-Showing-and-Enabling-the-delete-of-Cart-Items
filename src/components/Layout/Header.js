2bc0b1fd068425b55fe2545209fb443f4de4e539
Ankit Manjhi <ankitmanjhi286@gmail.com>
2024-01-29 15:49:59 +0530

    Cart open and close

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import HeaderCartButton from './HeaderCartButton';
 import Image from '../../assets/Image.png';
 import classes from './Header.module.css'
 
-const Header = props => {
+const Header = (props) => {
     return ( 
     <Fragment>
         <header className = {classes.header}>
             <h1>ReactMeals</h1>
-            
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
         <div className={classes ['main-image']}>
             <img src={Image} height="400px" alt="A table full of delicious food!" />
